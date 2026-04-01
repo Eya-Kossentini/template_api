91f044ea0cb1c08408edfb4178bdc9223ce5f2cc
Eya Kossentini <Kossentinieya2000@gmail.com>
2026-04-01 12:15:41 +0200

    update application

--- application.py
+++ application.py
@@ -5,6 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from admin import endpoints 
 
 from admin.machine_assets.machine_setup.defect_rate.endpoints import defect_rate_endpoint
+from admin.machine_assets.machine_setup.line_quality.endpoints import line_quality_endpoint
+
 from containers import Container
 
 
@@ -15,11 +17,13 @@ def create_app() -> FastAPI:
 
     # Register provider
     container.providers["kpi_defect_rate_service"] = container.kpi_defect_rate_service
-
+    container.providers["KPILineProductionQualityService"] = container.KPILineProductionQualityService
+    
     # Wire container
     container.wire(
         modules=[
-            "admin.machine_assets.machine_setup.defect_rate.endpoints.defect_rate_endpoint"
+            "admin.machine_assets.machine_setup.defect_rate.endpoints.defect_rate_endpoint",
+            "admin.machine_assets.machine_setup.line_quality.endpoints.line_quality_endpoint"
         ]
     )
 
@@ -27,7 +31,7 @@ def create_app() -> FastAPI:
 
     app.include_router(endpoints.router, prefix="/auth", tags=["auth"])
     app.include_router(defect_rate_endpoint.router)  
-
+    app.include_router(line_quality_endpoint.router)  
     return app
 
 
